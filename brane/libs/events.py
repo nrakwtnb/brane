844eb178a145e84a39363bf94ff2ccda0e71dee2
Noriaki Watanabe <noriaki.mathphys+git@gmail.com>
2022-06-08 04:23:08 +0900

    remove brane.lib.events.py

--- brane/libs/events.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from brane.core.hook import FunctionHook  # noqa: E402
-from brane.libs.hooks import check_path_existence, create_parent_directory  # noqa E402
-from brane.typing import *  # noqa: F401, F403
-
-BasicEvents.read_pre.add_hooks(FunctionHook(check_path_existence))
-BasicEvents.write_pre.add_hooks(FunctionHook(create_parent_directory))
-from brane.libs.hooks import check_path_existence, create_parent_directory  # noqa E402
